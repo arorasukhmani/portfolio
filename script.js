934bd451e8831ed1061138adac0c198baaef45a1
arorasukhmani <91721651+arorasukhmani@users.noreply.github.com>
2023-12-30 21:33:58 +0530

    added portfolio

---
+++ script.js
@@ -0,0 +1,64 @@
+// toggle icon navbar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+};
+
+
+// active page on NavBar
+
+let sections = document.querySelectorAll('section');
+let NavLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            NavLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+// Sticky NavBar
+
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+menuIcon.classList.remove('bx-x');
+navBar.classList.remove('active');
+
+};
+
+// scroll reveal
+ScrollReveal({ 
+    // reset: true,
+    distance: '800px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { orgin: 'top' });
+ScrollReveal().reveal('.home-img, .certificates-container, .portfolio-box, .contact form', { orgin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { orgin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { orgin: 'right' });
+
+//types js
+
+// const typed = new Typed('.multiple-text', {
+//     strings: ['Frontend Developer', 'Content writer','Reader', 'Student'],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop:true
+// });
